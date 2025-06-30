dcab0cdd3805195a9639ab839267fc58083d7e0b
Patricio <it1@gestionglobalprint.com>
2025-06-30 20:00:22 +0200

    Firebase set up.

---
+++ src/environments/environments.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDdN4Vmh15xncyvuSk5n03veKPIGR1zkyk",
+        authDomain: "airlinesmanager-9873d.firebaseapp.com",
+        projectId: "airlinesmanager-9873d",
+        storageBucket: "airlinesmanager-9873d.firebasestorage.app",
+        messagingSenderId: "753822936917",
+        appId: "1:753822936917:web:d9e07df29cd0a72277c9dc"
+    }
+};
