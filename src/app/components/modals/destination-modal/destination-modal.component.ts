38fe50bf4ce50d735e52164697845ef6bd0a2dbf
Patricio <it1@gestionglobalprint.com>
2025-06-30 21:31:22 +0200

    Create route modal added

--- src/app/components/modals/destination-modal/destination-modal.component.ts
+++ src/app/components/modals/destination-modal/destination-modal.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Route } from '../../../models/route.model';
+import { RouteService } from '../../../services/route.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-destination-modal',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule, MatDialogModule],
   templateUrl: './destination-modal.component.html',
   styleUrl: './destination-modal.component.scss'
 })
 export class DestinationModalComponent {
+  route: Route = {
+    id: undefined,
+    originIata: '',
+    originCity: '',
+    destinationIata: '',
+    destinationCity: '',
+    plane: '',
+    planeImage: '',
+    economyPrice: 0,
+    businessPrice: 0,
+    firstPrice: 0,
+    turnarounds: 0,
+    flights: 0,
+    comments: '',
+  };
 
+  constructor(
+    public dialogRef: MatDialogRef<DestinationModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Route,
+    private routeService: RouteService
+  ) {
+    this.route = { ...data };
+  }
+
+  async save() {
+    await this.routeService.updateRoute(this.route.id!, this.route);
+    this.dialogRef.close();
+  }
+
+  async delete() {
+    if (confirm('Are you sure you want to delete this route?')) {
+      await this.routeService.deleteRoute(this.route.id!);
+      this.dialogRef.close();
+    }
+  }
 }
